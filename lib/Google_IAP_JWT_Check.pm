03e6aef32a2275d9d2b3a0129183de2326029353
Kobayasi, Hiroaki <hkoba@ssri.com>
2023-05-09 14:41:30 +0900

    iap public key をキャッシュからロード（無ければ fetch）

--- lib/Google_IAP_JWT_Check.pm
+++ lib/Google_IAP_JWT_Check.pm
@@ -4,16 +4,38 @@ use strict;
 use warnings;
 use MOP4Import::Base::CLI_JSON -as_base
   , [fields =>
-     , [cache_dir => default => "var/cache"]
-     , [cache_file => default => "public_key-jwk.json"]
+     , [cache_file => default => "var/cache/public_key-jwk.json"]
      , [key_url => default => "https://www.gstatic.com/iap/verify/public_key-jwk"]
+     , qw(
+       _iap_public_key
+     )
    ]
   ;
+
+use File::Basename;
 
 use URI;
 use HTTP::Tiny;
 
-sub fetch_public_key {
+sub iap_public_key {
+  (my MY $self) = @_;
+  $self->{_iap_public_key} //= do {
+    if (-e $self->{cache_file}) {
+      $self->cli_read_file($self->{cache_file});
+    } else {
+      my ($ok, $err) = $self->fetch_iap_public_key;
+      unless (-e (my $dir = dirname($self->{cache_file}))) {
+        mkdir $dir or die "Can't mkdir $dir: $!";
+      }
+      open my $fh, '>', $self->{cache_file}
+        or Carp::croak "Can't write to $self->{cache_file}: $!";
+      print $fh $self->cli_encode_json($ok);
+      $ok;
+    }
+  };
+}
+
+sub fetch_iap_public_key {
   (my MY $self) = @_;
   my $response = HTTP::Tiny->new->request(GET => $self->{key_url});
   if ($response->{success}) {
