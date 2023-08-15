bdf5c320aefbaf4e5c305a2f96a01960c8b7086c
Kobayasi, Hiroaki <buribullet@gmail.com>
2023-08-15 17:17:47 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Plack::Middleware::Validate\_Google\_IAP\_JWT - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Middleware::Validate_Google_IAP_JWT;
+
+# DESCRIPTION
+
+Plack::Middleware::Validate\_Google\_IAP\_JWT is ...
+
+# LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kobayasi, Hiroaki <buribullet@gmail.com>
---
+++ lib/Plack/Middleware/Validate_Google_IAP_JWT.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::Validate_Google_IAP_JWT;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Validate_Google_IAP_JWT - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::Validate_Google_IAP_JWT;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Validate_Google_IAP_JWT is ...
+
+=head1 LICENSE
+
+Copyright (C) Kobayasi, Hiroaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kobayasi, Hiroaki E<lt>buribullet@gmail.comE<gt>
+
+=cut
+
