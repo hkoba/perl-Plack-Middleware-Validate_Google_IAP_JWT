8e4132915d3045faded3c516df286d35b648a44d
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-12-18 20:59:25 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 1b3670f..85c0c29 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,9 +2,9 @@ Revision history for Perl extension Plack-Middleware-Validate_Google_IAP_JWT
     
     {{$NEXT}}
     
    -0.03 2024-12-18T08:12:58Z
    +0.04 2024-12-18T11:59:15Z
     
    -    - Allow psgi tuple as the psgi app for debugging aid. See <https://hkoba.hatenablog.com/entry/perl-adventcal2024-1220>.
    +    - Allow plain array as the PSGI app for debugging aid. See <https://hkoba.hatenablog.com/entry/perl-adventcal2024-1220>.
     
     0.02 2024-12-18T05:31:10Z

--- lib/Plack/Middleware/Validate_Google_IAP_JWT.pm
+++ lib/Plack/Middleware/Validate_Google_IAP_JWT.pm
@@ -3,7 +3,7 @@ package Plack::Middleware::Validate_Google_IAP_JWT;
 use strict;
 use warnings;
 
-our $VERSION = "0.03";
+our $VERSION = "0.04";
 
 use MOP4Import::Base::CLI_JSON -as_base
   , [fields =>
